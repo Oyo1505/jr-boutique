a5aa6daffd72b454e74fec43c37eb669b1addd2e
Henri-Pierre <rigoulet.henri.pierre@gmail.com>
2023-02-26 21:05:51 +0100

    header

--- site/components/common/Navbar/Navbar.tsx
+++ site/components/common/Navbar/Navbar.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import { FC } from 'react';
 import Link from 'next/link';
 import { Container } from '@components/ui';
--- site/components/ui/Container/Container.tsx
+++ site/components/ui/Container/Container.tsx
@@ -17,7 +17,7 @@ const Container: FC<ContainerProps> = ({
 }) => {
   const Component: React.ComponentType<React.HTMLAttributes<HTMLDivElement>> = el as any;
 
-  return <Component className={cn(className, s.container)}>
+  return <Component className={cn(className, s.container, clean)}>
     {children}
   </Component>;
 };
